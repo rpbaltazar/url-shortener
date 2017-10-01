a96522b0c954c0b27e54accef3973ebf8ad28441
Rui Baltazar <rui.p.baltazar@gmail.com>
2017-10-02 00:23:17 +0800

    WIP - test failing

--- spec/lib/url_shortener/encoder_spec.rb
+++ spec/lib/url_shortener/encoder_spec.rb
@@ -4,5 +4,7 @@ require_relative '../../../lib/url_shortener/encoder'
 RSpec.describe UrlShortener::Encoder do
   it 'converts an integer into a string' do
     expect(UrlShortener::Encoder.encode(0)).to eq 'a'
+    expect(UrlShortener::Encoder.encode(10)).to eq 'k'
+    expect(UrlShortener::Encoder.encode(46)).to eq 'bu'
   end
 end
