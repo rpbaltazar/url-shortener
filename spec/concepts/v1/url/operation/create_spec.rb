7b83fdc24f11c2d2774b4f267cb6f5400f3c16c8
Rui Baltazar <rui.p.baltazar@gmail.com>
2017-10-01 23:46:14 +0800

    WIP - passing initial spec

--- spec/concepts/v1/url/operation/create_spec.rb
+++ spec/concepts/v1/url/operation/create_spec.rb
@@ -6,11 +6,12 @@ RSpec.describe V1::Url::Create do
   it 'creates a short url for it' do
     result = V1::Url::Create.(pass_params)
 
-    expect(result).to be_successful
+    expect(result).to be_success
     new_url_entry = result['model']
     expect(new_url_entry).to be_persisted
-    expect(new_url_entry).not.to be_nil
+    expect(new_url_entry).to_not be_nil
     expect(new_url_entry.full_url).to eq 'http://google.com'
-    expect(new_url_entry.short_code).not.to be_nil
+    # TODO: Check that the result is an ecoded string from the id
+    expect(new_url_entry.short_code).to_not be_nil
   end
 end
