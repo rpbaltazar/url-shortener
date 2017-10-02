feb029ce832a7d9512dc95ac22461e019b390d96
Rui Baltazar <rui.p.baltazar@gmail.com>
2017-10-03 00:24:45 +0800

    WIP - added sad path spec

--- spec/requests/v1/urls_creation_spec.rb
+++ spec/requests/v1/urls_creation_spec.rb
@@ -2,17 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'URLs Creation', type: :request do
   describe 'post create' do
-    let(:params) { { url: { full_url: 'http://dummyurl.com' } } }
     let(:request) { post '/v1/urls', params: params }
 
     before do
       request
     end
 
-    it 'returns a expected response code' do
-      expect(response.content_type).to eq('application/json')
-      expect(response).to have_http_status(:created)
-      expect(response.body).to eq Url.last.to_json
+    describe 'when params are valid' do
+      let(:params) { { url: { full_url: 'http://dummyurl.com' } } }
+
+      it 'returns a created response code with correct data' do
+        expect(response.content_type).to eq('application/json')
+        expect(response).to have_http_status(:created)
+        expect(response.body).to eq Url.last.to_json
+      end
+    end
+
+    describe 'when params are invalid' do
+      let(:params) { { url: { full_url: '' } } }
+
+      it 'returns a unprocessable_entity response code with errors' do
+        expect(response.content_type).to eq('application/json')
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.body).to eq({ errors: ["Full url can't be blank"] }.to_json)
+      end
     end
   end
 end
