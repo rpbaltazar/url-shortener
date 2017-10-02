e61953ae2dc4ba8f6df3686b4c7acfa005fde95c
Rui Baltazar <rui.p.baltazar@gmail.com>
2017-10-02 23:31:33 +0800

    WIP - simple ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1 do
-    resource :url, only: [:create]
+    resources :urls, only: [:create]
   end
 
   root to: 'home#index'
