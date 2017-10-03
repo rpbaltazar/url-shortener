146f6b1274f96109b809224792f2b8c06b3d1074
Rui Baltazar <rui.p.baltazar@gmail.com>
2017-10-04 01:01:40 +0800

    WIP - added url statistic model with storext attrs and wrote simple spec for url

---
+++ app/models/url_statistic.rb
@@ -0,0 +1,15 @@
+class UrlStatistic < ActiveRecord::Base
+  include Storext.model
+
+  belongs_to :url
+
+  store_attributes :data do
+    user String
+    user_agent String
+    host String
+    accept_language String
+    remote_addr String
+    latitude Float
+    longitude Float
+  end
+end
