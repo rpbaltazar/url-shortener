059c671df0ee2c5918d429a6784cfdf1c3f2a030
Rui Baltazar <rui.p.baltazar@gmail.com>
2017-10-04 19:21:13 +0800

    WIP - added happy path spec

---
+++ spec/concepts/v1/url_statistic/operation/create_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe V1::UrlStatistic::Create do
+  let(:params) do
+    {
+      url_statistic: {
+        user_agent: 'http://google.com',
+        user: '123',
+        host: 'localhost',
+        accept_language: 'en-us',
+        remote_addr: '127.0.0.1',
+        url_id: url_id
+      }
+    }
+  end
+
+  describe 'when the referenced url is valid' do
+    let(:url) do
+      result = V1::Url::Create.({ url: { full_url: 'http://google.com' } })
+      result['model']
+    end
+
+    let!(:url_id) do
+      url.id
+    end
+
+    it 'creates a statistic entry for short url' do
+      result = V1::UrlStatistic::Create.(params)
+
+      expect(result).to be_success
+      new_stat_entry = result['model']
+      expect(new_stat_entry).to be_persisted
+      expect(new_stat_entry.user_agent).to eq 'http://google.com'
+      expect(new_stat_entry.user).to eq '123'
+      expect(new_stat_entry.host).to eq 'localhost'
+      expect(new_stat_entry.accept_language).to eq 'en-us'
+      expect(new_stat_entry.remote_addr).to eq '127.0.0.1'
+      expect(new_stat_entry.url).to eq url
+    end
+  end
+end
