67790f7206ee6386ec752725fe6ec5dfe01135cd
Rui Baltazar <rui.p.baltazar@gmail.com>
2017-10-04 20:02:58 +0800

    WIP - added route and dummy controller

---
+++ app/controllers/v1/stats_controller.rb
@@ -0,0 +1,10 @@
+module V1
+  class StatsController < ApplicationController
+    def show
+      # TODO: Operation to fetch statistics
+      # TODO: Representer for statistics
+      # TODO: Render json
+      render json: {}, status: :ok
+    end
+  end
+end
